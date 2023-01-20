4fcb0d5b781c3a4bacf848cef255614d365a88c3
Lucas Teixeira Ronchi <lucas0headshot@gmail.com>
2023-01-20 16:45:18 -0300

    Aula 04 - Aprimorando a App com JS
    Criei funções, condições e um objeto para entender como o JS funciona.
    Aprendi a usar o LocalStorage do navegador e passei a armazenar os dados, estou ancioso para aprender a hospedar na "nuvêm"

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+//Constantes
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-20', '01-21', '01-22', '01-23', '01-24', '01-25', '01-26', '01-27', '01-28', '01-29'],
-    takePills: ['01-20'],
-    journal: ['01-20']
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+
+//Funções
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists){
+        alert('Dia já incluso! 🔴')
+        return
+    }
+
+    alert('Dia adicionado! 🟢')
+    nlwSetup.addDay(today)
 }
 
+function save(){
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
 
@@ -24,4 +44,8 @@ nlwSetup.load()
 //     }
 // }
 
-// celular.ligar()
+// celular.ligar()// const data = {
+//     run: ['01-20', '01-21', '01-22', '01-23', '01-24', '01-25', '01-26', '01-27', '01-28', '01-29'],
+//     takePills: ['01-20'],
+//     journal: ['01-20']
+// }
